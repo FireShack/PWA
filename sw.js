0f9011711ff2a5422d54ee89e5dcbf2b0836ebf2
Franco <francoguardini10@gmail.com>
2022-01-28 19:04:41 -0300

    some new changes

--- sw.js
+++ sw.js
@@ -1,11 +1,39 @@
+// All archives
+const cacheName = 'apv-v1'
+const archives = [
+    '/',
+    'index.html',
+    'css/bootstrap.css',
+    'css/styles.css',
+    'js/apv.js',
+    'js/app.js'
+]
+
 self.addEventListener('install', e => {
     console.log('Installed')
+
+    e.waitUntil(
+        caches.open(cacheName)
+        .then(Cache => {
+            console.log('Registrando Cache')
+            Cache.addAll(archives)
+        })
+
+    )
+
 })
 
 self.addEventListener('activate', e => {
     console.log('activated')
 })
 
-self.addEventListener('fetch', ev => {
+self.addEventListener('fetch', e => {
     // console.log(ev)
+
+    e.respondWith(
+        caches.match(e.request)
+        .then(result => {
+            return result
+        })
+    )
 })
